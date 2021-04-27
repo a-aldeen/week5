372ab19dded353c79cc575fc39abeb1c169472da
Alaa <alaa.aldeen@kellogg.northwestern.edu>
2021-04-27 10:20:47 -0500

    modified

--- code-along/events-practice.js
+++ code-along/events-practice.js
@@ -9,29 +9,47 @@ function getRandomInt(max) {
 // RECIPE (algorithm) – do not modify the HTML!
 
 // Store a reference to the roll button in memory
-
+let rollButton = document.querySelector(`#roll-button`)
 // Listen for the clicking of the roll button, when clicked:
+rollButton.addEventListener(`click`, async function () {
 
-  // - Ignore the roll button's default behavior
+// - Ignore the roll button's default behavior
+event.preventDefault()
 
+  
   // - Store a random integer in memory, representing the roll of the first die
+  randomInteger1 = getRandomInt(6)
   
   // - Store a random integer in memory, representing the roll of the second die
-
+  randomInteger2 = getRandomInt(6)
+console.log(`The second random is ${randomInteger2}`)
   // - Store the total of the two random numbers, representing the total roll
+  total = randomInteger1 + randomInteger2
 
   // - Replace the first die image using the first random value (hint: setAttribute)
+  let firstImage = document.querySelector(`.die1`)
+  firstImage.setAttribute(`src`, `../images/dice/${randomInteger1}.png`)
 
   // - Replace the second die image using the second random value (hint: setAttribute)
+  let secondImage = document.querySelector(`.die2`) 
+  secondImage.setAttribute(`src`, `../images/dice/${randomInteger2}.png`)
 
   // - Store a reference to the player name input element
-
-  // - Grab the value of the player name element and store the player's name in memory
+  let nameInput = document.querySelector(`#player`)
   
+  // - Grab the value of the player name element and store the player's name in memory
+  let playerNmae = nameInput.value 
+
   // - Make sure the player's name is filled out; if it is:
+  if (playerNmae.length>0) {
 
     // - Form a sentence in memory, containing the player's name and the total that was rolled
+    let sentence = (`The first name is ${playerNmae} and his total is ${total}`)
 
     // - Store a reference to the "result" un-ordered list element
+    let playerResult = document.querySelector(`.result`)
 
     // - Insert the sentence to the "result" un-ordered list as a list item (li)
+    playerResult.innerHTML = sentence
+  }
+})
--- code-along/events.js
+++ code-along/events.js
@@ -12,8 +12,94 @@ movieList.insertAdjacentHTML(`beforeend`, `
   <li>Spaceballs</li>
 `)
 
-// Get a reference to the image
-let image = document.querySelector(`img`)
 
-// Add the `border-pink-500` class to the image element
-image.classList.add(`border-pink-500`)
+// Get a referernce for the click me button
+let clickMeButton = document.querySelector(`.click-me`)
+
+// Event listener for the click me button
+clickMeButton.addEventListener(`click`, async function() {
+  alert(`the button was clicked`)
+  let pageTitle = document.querySelector(`.page-title`)
+  pageTitle.insertAdjacentHTML(`beforeend`, `No Titlesss`)
+})
+
+// Get a reference for the add movie link
+let addMovieLink = document.querySelector(`.add-movie`)
+
+//Build an event listener for teh add link
+addMovieLink.addEventListener(`click`, async function () {
+
+  // prevent the link default behavior
+  event.preventDefault()
+
+  // do a new thing - which is add a movie to the list
+let moviesNames = document.querySelector(`.movies-to-watch`)
+movieList.insertAdjacentHTML(`beforeend`, `
+<li> New Movie Whatever</li>
+`)
+})
+
+// Get a reference to the zoom image button
+let zoomImageButton = document.querySelector(`.zoom-image`)
+
+// Add event listener
+zoomImageButton.addEventListener(`click`, async function() {
+
+  // find the image
+  let imageInPage = document.querySelector(`img`)
+
+  // Add the `border-pink-500` class to the image element
+  imageInPage.classList.add(`border-pink-500`)  
+
+  // make the image larger
+  imageInPage.classList.toggle(`w-96`)
+  imageInPage.classList.toggle(`w-full`)
+})
+
+// Get a reference to the change image button
+let changeImageButton = document.querySelector(`.change-image`)
+
+// Add an event listener
+changeImageButton.addEventListener(`click`, async function () {
+
+  // find the image 
+  let newImage = document.querySelector(`img`)
+
+  //change the image source
+  newImage.setAttribute(`src`, `../images/grogu2.jpg`)
+
+})
+
+// Get a reference fo teh say hi button
+let sayHiButton = document.querySelector(`.say-hi`)
+
+// Add event listener
+sayHiButton.addEventListener(`click`, async function () {
+
+  // prevent the default behavior
+  event.preventDefault()
+
+  // add action
+  console.log(`The form was submitted`)
+
+  // get a reference to the input field that holds the first name (id is done with # class is done with a dot)
+  let nameInput = document.querySelector(`#first-name`)
+
+  // get the value from teh input field
+let firstName = nameInput.value
+
+  // get the refernce to greet
+  let Greet = document.querySelector(`.greet`)
+  
+  // first name should have value
+  if (firstName.length>0) {
+  // create a sentece to put in the greet element
+  let sentence = `Hi, ${firstName}`
+
+  // set teh greet element html to the sentence
+  Greet.innerHTML = sentence
+  }
+  else {
+  Greet.innerHTML = ``
+  }
+})
